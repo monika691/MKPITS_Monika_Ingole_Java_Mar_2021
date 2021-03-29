884fd380f059e0b2c4e0fa240916f83902a6132b
monika691 <monikaingole06@gmail.com>
2021-03-29 15:27:57 +0530

    forloop,switch inside

---
+++ C_Programs/27_03_natno_frlp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+    printf("enter number\n");
+    scanf("%d",&num);
+
+    if(num>0){
+        printf("it is natural number\n");
+            }else {
+                printf("not a natural number\n");
+            }
+    return 0;
+}
