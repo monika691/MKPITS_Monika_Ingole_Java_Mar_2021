8623455b70e1af4cb227ea9daa7e812405e44e07
monika691 <monikaingole06@gmail.com>
2021-04-03 20:25:28 +0530

    structure and union

--- C_Programs/2_04_structure_totalpergrade.c
+++ C_Programs/2_04_structure_totalpergrade.c
@@ -1,46 +1,53 @@
 //7 create a structure student having members rno,name ,marks1,marks2 ,marks3 ,total, percentage
 //calculate total , percentage and grade.
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
+struct student
+{
+    int rollno, m1, m2, m3;
 
+    char name[20];
+
+} s1;
 int main()
 {
-    struct student
-{
-    int rollno;
-    int marks;
-    int total;
-    char name[20];
     float per;
-}stud1;
-        printf("Enter roll no.\n");
-        scanf("%d",&stud1.rollno);
-        rintf("Enter name\n");
-        scanf("%s",&stud1.name);
-        rintf("Enter  3 smarks\n");
-        scanf("%d%d%d",&stud1.rollno,&stud1.rollno,&stud1.rollno);
-        
-stud1.total =stud1.total+stud1.marks;
-   printf("total=%d\n",stud1.total);
-  
-   stud1.per = (float)(stud1.total/300.0f)*100.0f;
-   printf("per=%f\n",stud1.per);
+    int total;
+    printf("Enter roll no.\n");
+    scanf("%d", &s1.rollno);
+    printf("Enter name\n");
+    scanf("%s", &s1.name);
+    printf("Enter  1 marks\n");
+    scanf("%d",&s1.m1);
+    printf("Enter  2 marks\n");
+    scanf("%d",&s1.m2);
+    printf("Enter  3 marks\n");
+    scanf("%d",&s1.m3);
 
-   if(per>75)
-   {
-       printf("grade:distinction\n");
-   }else if(per>=60 && per<75)
-   {
-       printf("grade:first class\n");
-   }else if(per>=40 && per <60)
-   {
-       printf("grade:secound class\n");
-   }else{
-       printf("fail\n");
-   }
-   
-   
-   
+    total = s1.m1 + s1.m2 + s1.m3;
+    per = (float)(total / 300.0f) * 100.0f;
+    printf("rollno =%d\n", s1.rollno);
+    printf("name=%s\n", s1.name);
+    printf("total=%d\n", total);
+    printf("per=%f\n", per);
+
+    if (per > 75)
+    {
+        printf("grade:distinction\n");
+    }
+    else if (per >= 60 && per < 75)
+    {
+        printf("grade:first class\n");
+    }
+    else if (per >= 40 && per < 60)
+    {
+        printf("grade:secound class\n");
+    }
+    else
+    {
+        printf("fail\n");
+    }
+
     return 0;
 }
