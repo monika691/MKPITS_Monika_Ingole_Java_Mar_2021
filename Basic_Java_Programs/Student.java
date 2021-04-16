f692d9a4ccb0b52090ee01eef701e1bffb84d3fc
monika691 <monikaingole06@gmail.com>
2021-04-16 11:06:12 +0530

    field,construct example

---
+++ Basic_Java_Programs/Student.java
@@ -0,0 +1,17 @@
+/*1-create a class student having fields id and name
+
+ */
+public class Student {
+        int id;
+        String name;
+
+        public static void main(String[] arg)
+        {
+            Student s1=new Student();
+            s1.id=123;
+            s1.name="monika";
+            System.out.println("Student id : " + s1.id);
+            System.out.println("Student name : " +s1.name);
+        }
+    }
+
