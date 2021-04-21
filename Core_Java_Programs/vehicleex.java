112c08225f59e503d7c455dc1df085f29c407189
monika691 <monikaingole06@gmail.com>
2021-04-21 19:14:33 +0530

    20 Apr 21, Method overriding example

---
+++ Core_Java_Programs/vehicleex.java
@@ -0,0 +1,19 @@
+package com.mkpits.java.methodoverriding;
+
+
+class Vehicle{
+    void run(){
+        System.out.println("vehicle is running");
+    }
+}
+class bike extends Vehicle
+        { void run() {
+            System.out.println("bike is running safely");
+        }
+        }
+public class vehicleex {
+    public static void main(String[] args) {
+        bike b=new bike();
+        b.run();
+    }
+}
